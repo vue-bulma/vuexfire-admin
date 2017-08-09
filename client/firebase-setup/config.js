92e8ca0240a9c4551f07a5c0234c6148153cca38
tdaff <tdaff@digitalink.com.au>
2017-08-09 12:39:11 +1000

    Added firebase-config

---
+++ client/firebase-setup/config.js
@@ -0,0 +1,9 @@
+export default {
+  // Firebase Config
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_PROJECT_ID,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID
+}
