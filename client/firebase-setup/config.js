87cc5bf18d8b59f1160322976a672c5b267bb54f
tdaff <tdaff@digitalink.com.au>
2017-08-21 13:41:22 +1000

    Fixed call to incorrect config preventing auth working

--- client/firebase-setup/config.js
+++ client/firebase-setup/config.js
@@ -1,7 +1,7 @@
 export default {
   // Firebase Config
   apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_PROJECT_ID,
+  authDomain: process.env.FB_AUTH_DOMAIN,
   databaseURL: process.env.FB_DATABASE_URL,
   projectId: process.env.FB_PROJECT_ID,
   storageBucket: process.env.FB_STORAGE_BUCKET,
